b16acdcfc8f02d6edc3cddc5c1e5aa3877c89982
prettynatty <fear@loathing.in>
2015-10-02 20:10:51 +0300

    init

---
+++ build.sbt
@@ -0,0 +1,26 @@
+import com.trueaccord.scalapb.{ScalaPbPlugin => PB}
+
+PB.protobufSettings
+
+PB.runProtoc in PB.protobufConfig := (args => com.github.os72.protocjar.Protoc.runProtoc("-v300" +: args.toArray))
+
+scalaVersion := "2.11.7"
+
+val akkaV = "2.3.13"
+
+resolvers ++= Seq(
+  Resolver.sonatypeRepo("releases"),
+  Resolver.bintrayRepo("jdgoldie", "maven")
+)
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % akkaV,
+  "com.typesafe.akka" %% "akka-persistence-experimental" % akkaV,
+  "com.typesafe.akka" %% "akka-contrib" % akkaV,
+  "com.typesafe.akka" %% "akka-testkit" % akkaV,
+  "com.eaio.uuid" % "uuid" % "3.4",
+  "com.google.protobuf" % "protobuf-java" % "3.0.0-alpha-3",
+  "im.actor" %% "akka-scalapb-serialization" % "0.1.3",
+  "org.scalatest" %% "scalatest" % "2.2.4" % "test",
+  "com.github.jdgoldie" %% "akka-persistence-shared-inmemory" % "1.0.16" % "test"
+)
---
+++ src/test/scala/shardakka/KeyValueSpec.scala
@@ -0,0 +1,98 @@
+package shardakka
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import akka.util.Timeout
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Seconds, Span}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfter, FlatSpecLike, Matchers}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+
+final class KeyValueSpec extends TestKit(ActorSystem())
+with FlatSpecLike
+with ScalaFutures
+with Matchers
+with BeforeAndAfterAll {
+
+  it should "set and get values" in setAndGet
+  it should "get keys lsit" in keysList
+  it should "restore state" in restoreState
+  it should "upsert and delete" in upsertAndDelete
+
+  implicit val timeout = Timeout(5.seconds)
+
+  override implicit def patienceConfig: PatienceConfig =
+    new PatienceConfig(timeout = Span(5, Seconds))
+
+  private implicit val ec: ExecutionContext = system.dispatcher
+
+  val ext = ShardakkaExtension(system)
+
+  def setAndGet() = {
+    val keyValue = ext.simpleKeyValue("setAndGet")
+
+    whenReady(keyValue.get("key1")) { resp ⇒
+      resp shouldBe empty
+    }
+
+    whenReady(keyValue.upsert("key1", "value"))(identity)
+
+    whenReady(keyValue.get("key1")) { resp ⇒
+      resp shouldBe Some("value")
+    }
+  }
+
+  def keysList() = {
+    val keyValue = ext.simpleKeyValue("keysList")
+
+    whenReady(Future.sequence(Seq(
+      keyValue.upsert("key1", "value"),
+      keyValue.upsert("key2", "value"),
+      keyValue.upsert("key3", "value")
+    )))(identity)
+
+    whenReady(keyValue.getKeys()) { keys ⇒
+      keys.toSet shouldBe Set("key1", "key2", "key3")
+    }
+  }
+
+  def restoreState() = {
+    val kvName = "restoreState"
+
+    val keyValue = ext.simpleKeyValue(kvName)
+
+    whenReady(keyValue.upsert("key1", "value"))(identity)
+
+    ext.shutdownKeyValue(kvName)
+    Thread.sleep(200)
+
+    val keyValueNew = ext.simpleKeyValue(kvName)
+
+    whenReady(keyValueNew.get("key1")) { resp ⇒
+      resp shouldBe Some("value")
+    }
+  }
+
+  def upsertAndDelete() = {
+    val keyValue = ext.simpleKeyValue("upsertAndDelete")
+
+    whenReady(keyValue.upsert("key1", "value"))(identity)
+
+    whenReady(keyValue.get("key1")) { resp ⇒
+      resp shouldBe Some("value")
+    }
+
+    whenReady(keyValue.delete("key1"))(identity)
+
+    whenReady(keyValue.get("key1")) { resp ⇒
+      resp shouldBe empty
+    }
+  }
+
+  override def afterAll = {
+    system.shutdown()
+    system.awaitTermination()
+  }
+}
