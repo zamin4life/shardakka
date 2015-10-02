9ca020ca1f053e13c75afaebe5628bbe268bb6eb
prettynatty <fear@loathing.in>
2015-10-02 21:37:02 +0300

    attempt to reach java-compatibility

--- src/main/scala/shardakka/ShardakkaExtension.scala
+++ src/main/scala/shardakka/ShardakkaExtension.scala
@@ -4,18 +4,17 @@ import akka.actor._
 import shardakka.keyvalue.SimpleKeyValueExtension
 import scala.concurrent.duration._
 
-trait ShardakkaExtension extends Extension with SimpleKeyValueExtension
-
-final class ShardakkaExtensionImpl(_system: ExtendedActorSystem) extends ShardakkaExtension {
+final class ShardakkaExtension(_system: ExtendedActorSystem) extends Extension with SimpleKeyValueExtension {
   private implicit val system: ActorSystem = _system
-
 }
 
 object ShardakkaExtension extends ExtensionId[ShardakkaExtension] with ExtensionIdProvider {
   val CacheTTL = 5.minutes
   val KVPersistencePrefix = "kv"
 
-  override def createExtension(system: ExtendedActorSystem): ShardakkaExtension = new ShardakkaExtensionImpl(system)
+  override def createExtension(system: ExtendedActorSystem): ShardakkaExtension = new ShardakkaExtension(system)
 
   override def lookup(): ExtensionId[_ <: Extension] = ShardakkaExtension
+
+  override def get(system: ActorSystem): ShardakkaExtension = super.get(system)
 }
